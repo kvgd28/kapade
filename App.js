e6dd7acfda173e1c0c8978d624b759719e3e564b
kvgd28 <kvgd28@gmail.com>
2022-05-01 01:06:42 +0530

    9 files added, 2 deleted

---
+++ App.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
+import Home from './components/pages/Home';
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/contact-us" component={ContactUs} />
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/marketing" component={Marketing} />
+        <Route path="/consulting" component={Consulting} />
+      </Switch>
+    </Router>
+  );
+}
