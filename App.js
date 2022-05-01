f412bad7111a700175512b824501bffcdbdcba7d
kvgd28 <kvgd28@gmail.com>
2022-05-01 23:55:49 +0530

    Initial version of the app

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import GlobalStyle from './globalStyles';
-import Home from './pages/HomePage/Home';
-import Services from './pages/Services/Services';
-import Products from './pages/Products/Products';
-import SignUp from './pages/SignUp/SignUp';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
-import { Navbar, Footer } from './components';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <Router>
-      <GlobalStyle />
-      <ScrollToTop />
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/services" component={Services} />
-        <Route path="/products" component={Products} />
-        <Route path="/sign-up" component={SignUp} />
-      </Switch>
-      <Footer />
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
